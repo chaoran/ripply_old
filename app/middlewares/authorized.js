519a5f7a8fbea9156b9c5088fa50ae11bd70d97a
Chaoran Yang <chaorany@me.com>
2013-07-11 10:08:02 -0500

    Change error message for lack of permission

--- app/middlewares/authorized.js
+++ app/middlewares/authorized.js
@@ -3,11 +3,16 @@ var Token = require('../models/token');
 module.exports = {
   post: function(req, res, next) {
     if (req.token.permissions.indexOf('post') >= 0) next();
-    else res.send(401, { error: "lack_permission", });
+    else res.send(401, { 
+      error: "invalid_scope", 
+      message: "access token lacks proper scope"
+    });
   },
   register: function(req, res, next) {
     if (req.client.trusted === true) next();
     else res.send(403, { 
+      error: "forbidden", 
+      message: "url is reserved for internal use"
     });
   }
 };
